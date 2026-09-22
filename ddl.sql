1bba3d877668c6402aab7807a0af75294cb2dcfb
marchioreleo <leocaninamarchiori@gmail.com>
2026-09-22 17:21:39 -0300

    up

---
+++ ddl.sql
@@ -0,0 +1,51 @@
+drop database if exists estoque;
+
+create database estoque;
+
+use estoque;
+
+create table produto(
+    id int primary key auto_increment not null,
+    nome varchar(100) not null,
+    descricao varchar(200),
+    preco decimal(10,2) not null,
+    marca varchar(50) not null,
+    id_categoria int not null,
+    id_fornecedor int not null
+);
+
+create table categoria(
+    id int primary key auto_increment not null,
+    nome varchar(100) not null,
+    descricao text
+);
+
+create table fornecedor(
+    id int primary key auto_increment not null,
+    razao_social varchar(50) not null,
+    nome_fantasia varchar(50) not null,
+    cnpj varchar(20) not null,
+    telefone varchar(16) not null,
+    email varchar(50) not null,
+    endereco varchar(100) not null
+);
+
+create table estoque(
+    id_estoque int not null,
+    id_produto int not null,
+    quantidade int not null,
+    quantidade_minima int not null,
+    localizacao varchar(50) not null
+);
+
+create table movimentacao(
+    id_movimentacao int not null,
+    id_produto int not null,
+    tipo enum('ENTRADA', 'SAIDA'),
+    quantidade int not null,
+    data datetime not null default(curtime())
+);
+
+alter table produto add constraint fk_categoria foreign key (id_categoria) references categoria(id);
+alter table produto add constraint fk_fornecedor foreign key (id_fornecedor) references fornecedor(id);
+alter table estoque add constraint fk_produto foreign key (id_produto) references produto(id);
---
+++ dml.sql
@@ -0,0 +1,26 @@
+use estoque;
+
+insert into produto(nome, descricao, preco, marca, id_categoria, id_fornecedor) values
+("Sallet", "Italian 15th-century helmet", 2000, "Giovanni's smithy", 1, 1),
+("Cuirass", "Italian 15th-century main chest armour", 4000, "Mario's little forge", 1, 2),
+("Sabaton", "Italian 15th-century armoured boots", 1200, "Luigi's shop", 1, 3);
+
+insert into categoria(nome, descricao) values
+("Upper body", "Components of the upper part of an armour"),
+("Central piece", "Components of the central part of an armour"),
+("Lower body", "Components of the lower part of an armour");
+
+insert into fornecedor(razao_social, nome_fantasia, cnpj, telefone, email, endereco) values
+("Giovanni's.ltd", "Giovanni's smithy", "53.673,123/0001-10", "(39)12942-1583", "giovannismithy@email.com", "Passo dello Stelvio - 1" ),
+("Mario's.ltd", "Mario's little forge", "34.145.102/0001-99", "(39)81924-1567", "mariolittleforge@email.com", "Grande Strada delle Dolomiti - 67" ),
+("Luigi's.ltd", "Luigi's shop", "42.783.567/0001-45", "(39)44128-1532", "luigishop@email.com", "Strada della Forra - 30" );
+
+insert into estoque(id_estoque, id_produto, quantidade, quantidade_minima, localizacao) values
+(1, 10, 15, 1, "Lombardia"),
+(2, 20, 7, 1, "Veneto"),
+(3, 30, 20, 1, "Lombardia");
+
+insert into movimentacao(id_movimentacao, id_produto, tipo, quatidade, data) values
+(1, 1, "ENTRADA", 4, null),
+(2, 2, "SAIDA", 3, null),
+(3, 3, "SAIDA", 6, null);
